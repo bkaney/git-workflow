5c3adc269d57582b5faca5f67689166f45d5afbc
Brian Kaney <brian@vermonster.com>
2010-01-20 14:45:00 -0500

    [#29] Test coverage on building

---
+++ test/building_test.rb
@@ -0,0 +1,14 @@
+require 'test/unit'
+
+class HelloTest < Test::Unit::TestCase
+  # def setup
+  # end
+
+  # def teardown
+  # end
+
+  def test_pass
+    assert(true, 'Assertion should be true')
+  end
+
+end
