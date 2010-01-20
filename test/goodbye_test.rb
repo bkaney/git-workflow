1182f6e792aff1f2a88f5ef14523a43b7d2f65a2
Brian Kaney <brian@vermonster.com>
2010-01-20 14:42:11 -0500

    [#19] Forgot to add test coverage

---
+++ test/goodbye_test.rb
@@ -0,0 +1,14 @@
+require 'test/unit'
+
+class GoodbyeTest < Test::Unit::TestCase
+  # def setup
+  # end
+
+  # def teardown
+  # end
+
+  def test_pass
+    assert(true, 'Assertion should be true')
+  end
+
+end
